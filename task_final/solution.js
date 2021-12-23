8749ab5add0a6653f1a5eb63f25e378837d527e4
CatWool <96100834+CatWool@users.noreply.github.com>
2021-12-23 13:51:18 +0400

    Update solution.js
    Final home task.

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,18 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {data: {client: "", order: {address: "", sum: 0}, goods: []}};
+
+    data.data.client = name + " " + phone;
+    data.data.order.address = 'ул. ' + address.street + ', дом ' + address.house+ ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    data.data.order.sum = sum;
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i++) {
+        let good = {};
+        good.title = goods[i].title;
+        good.count = goods[i].count;
+
+        data.data.goods.push(good);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
 
     let jsonData = JSON.stringify(data);
 
